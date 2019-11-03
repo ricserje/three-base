37cbec18f24fb4da6aa11cafe87fa2cb7edbe57e
ric <ricserje@hotmail.com>
2019-11-02 19:02:53 -0500

    base config

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const aspect = window.innerWidth / window.innerHeight;
+const camera = new THREE.PerspectiveCamera( 75, aspect, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshNormalMaterial();
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+camera.position.z = 5;
+
+const render = function () {
+  requestAnimationFrame( render );
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render( scene, camera );
+};
+
+render();
